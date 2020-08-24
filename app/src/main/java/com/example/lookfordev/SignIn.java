77930a0ba0b11d675bbbd30a546b84406883feef
Viraj Shah <virajshah.vs30@gmail.com>
2020-08-24 20:29:12 +0530

    Login Completed
    Register link left
    Logout button left

--- app/src/main/java/com/example/lookfordev/SignIn.java
+++ app/src/main/java/com/example/lookfordev/SignIn.java
@@ -1,14 +1,96 @@
 package com.example.lookfordev;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignIn extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    EditText email,password;
+    TextView forgotpass;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        forgotpass = findViewById(R.id.forgotpass);
+        login = findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail,pass;
+                mail = email.getText().toString();
+                pass = password.getText().toString();
+                if (TextUtils.isEmpty(mail)) {
+                   // Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+                    email.setError( "Email is required!" );
+                }
+                else if (TextUtils.isEmpty(pass)) {
+                    //Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+                    password.setError( "Password is required!" );
+                }
+                else
+                {
+                    mAuth.signInWithEmailAndPassword(mail, pass)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(SignIn.this, DevDashboard.class);
+                                        startActivity(intent);
+                                    }
+                                    else {
+                                        Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+        forgotpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail;
+                mail = email.getText().toString();
+                if (TextUtils.isEmpty(mail)) {
+                    //Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+                    email.setError( "Email is required!" );
+                }
+                else
+                {
+                    mAuth.sendPasswordResetEmail(mail)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Mail for password reset sent", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Reset failed! Please try again later", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
     }
 }
