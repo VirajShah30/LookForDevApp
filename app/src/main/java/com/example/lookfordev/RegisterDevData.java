9b710957067d08c99a38c870a9c9c648f29d3de2
jashmehta21 <70139909+jashmehta21@users.noreply.github.com>
2020-09-01 15:42:33 +0530

    Developer/Company Register page
    completed company and developer side register page

--- app/src/main/java/com/example/lookfordev/RegisterDevData.java
+++ app/src/main/java/com/example/lookfordev/RegisterDevData.java
@@ -3,6 +3,8 @@ package com.example.lookfordev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegisterDevData extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RegisterDevData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_dev_data);
+
+        Spinner dropdown = findViewById(R.id.spinner1);
+        String[] items = new String[]{"Empty", "Web Developer", "App Developer", "Hybrid Developer", "UI/UX Developer", "Backend Developer"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
     }
 }
